b3e69e24d70d21e4018b51b9b2dbf972486b2e4d
MAC <mac@KashParas-Macbook-Pro-13.local>
2024-08-26 10:57:07 +0100

    new

---
+++ module.py
@@ -0,0 +1,6 @@
+import mymodule
+mymodule.greeting("Jonathan")
+
+import mymodule as mm
+a = mm.person1['age']
+print(a)
---
+++ show.py
@@ -0,0 +1,22 @@
+myShow = ["Mike", "", "Emma", "Kelly", "", "Brand"]
+myShow= list(filter(None, myShow))
+print(myShow)
+
+
+
+list4 = [5, 10, 15, 20, 25, 50, 20]
+
+# Find the index of the first occurrence of 20
+index = list4.index(20)
+
+# Replace the value at the found index with 200
+list4[index] = 200
+
+print(list4)
+
+thisdict ={
+    "items": "spoon, food, plate",
+    "food": "rice, beans"
+}
+thisdict
+
